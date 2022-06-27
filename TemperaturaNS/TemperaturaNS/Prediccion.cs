f8a1f1309443d62c4bdaa5169ea296f46b217040
Desiree Moya <desmp7@gmail.com>
2022-06-27 19:43:34 +0200

    Proyecto acabado

---
+++ TemperaturaNS/TemperaturaNS/Prediccion.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturaNS
+{
+    public class Prediccion
+    {
+        public const string NoListaVacia1 = "Faltan datos en el día 1";
+        public const string NoListaVacia2 = "Faltan datos en el día 2";
+        public const string NoListaVacia3 = "Faltan datos en el día 3";
+
+        private double tempCelsius; // temperatura en grados Celsius
+        private double tempFarenheit; // temperatura en grados farenheit
+        private double tempMax; // temperatura máxima
+        private double tempMin; // temperatura mínima
+
+        /// <summary>
+        /// Getters y setters de las variables
+        /// </summary>
+        public double TempCelsius { get => tempCelsius; set => tempCelsius = value; }
+        public double TempFarenheit { get => tempFarenheit; set => tempFarenheit = value; }
+        public double TempMax { get => tempMax; set => tempMax = value; }
+        public double TempMin { get => tempMin; set => tempMin = value; }
+
+
+        /// <summary>
+        /// Constuctor que inicializan las variables
+        /// </summary>
+        public Prediccion()
+        {
+            TempCelsius = 0;
+            TempFarenheit = 0;
+            TempMax = -1000;
+            TempMin = 1000;
+        }
+
+        /// <summary>
+        /// Funcion que predice la temperatura a raiz de tres dias de observacion
+        /// <para>calculamos la temperatura media total, dándo más peso al último día que al primero</para>
+        /// </summary>
+        /// <param name="observacionDia1">datos de orsevacion de dia 1</param>
+        /// <param name="observacionDia2">datos de orsevacion de dia 2</param>
+        /// <param name="observacionDia3">datos de orsevacion de dia 3</param>
+        /// <exception cref="Exception">La lista no puede estar vacía</exception>
+        /// <returns>devuelve la prevision basada en el metodo</returns>  
+
+        public bool PredecirTemperatura(List<double> observacionDia1,
+            List<double> observacionDia2, List<double> observacionDia3)
+        {
+            int i;
+            double tempMedia1 = 0,
+                   tempMedia2 = 0,
+                   tempMedia3 = 0; // temperatura media de cada día
+
+            if (observacionDia1.Count <= 0)
+            {
+                throw new Exception(NoListaVacia1);
+                // Tenemos que tener al menos una observación
+            }
+            i = CalcularPrevision(observacionDia1, ref tempMedia1);
+
+            tempMedia1 = tempMedia1 / observacionDia1.Count;
+
+
+
+            if (observacionDia2.Count <= 0)
+            {
+                     throw new Exception(NoListaVacia2);
+                // Tenemos que tener al menos una observación
+            }
+
+            i = CalcularPrevision(observacionDia2, ref tempMedia2);
+
+            tempMedia2 = tempMedia2 / observacionDia2.Count;
+
+            if (observacionDia3.Count <= 0)
+            {
+                    throw new Exception(NoListaVacia3);
+                // Tenemos que tener al menos una observación
+            }
+
+            i = CalcularPrevision(observacionDia3, ref tempMedia3);
+
+            tempMedia3 = tempMedia3 / observacionDia3.Count;
+
+             TempCelsius = 0.2 * tempMedia1 + 0.35 * tempMedia2 + 0.45 * tempMedia3;
+             TempFarenheit = (TempCelsius * 1.8) + 32;
+            // calculamos también la temperatura en grados farenheit
+
+            return true;
+        }
+
+        private int CalcularPrevision(List<double> observacionDia1, ref double tempMedia1)
+        {
+            int i;
+            for (i = 0; i < observacionDia1.Count; i++)
+            {
+                tempMedia1 = tempMedia1 + observacionDia1[i];
+
+                if (TempMin > observacionDia1[i])
+                {
+                    TempMin = observacionDia1[i];
+                }
+
+                if (TempMax < observacionDia1[i])
+                {
+                    TempMax = observacionDia1[i];
+                }
+            }
+            return i;
+        }
+    }
+}
--- TemperaturaNS/TemperaturaPrueba/UnitTest1.cs
+++ TemperaturaNS/TemperaturaPrueba/UnitTest1.cs
@@ -9,8 +9,98 @@ namespace TemperaturaPrueba
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaDatosCorrectos() //Esta prueba pretende sacar un error si la temperatura máxima o mínima es mayor o menor de lo introducido en la clase
         {
+            Prediccion miPrediccionPrueba = new Prediccion();
+            List<double> dia1 = new List<double>(),
+                         dia2 = new List<double>(),
+                         dia3 = new List<double>();
+
+            dia1.Add(12.5);
+            dia1.Add(16.5);
+            dia1.Add(21);
+            dia1.Add(17);
+            dia1.Add(15);
+            dia2.Add(13);
+            dia2.Add(15);
+            dia2.Add(19.5);
+            dia2.Add(16.5);
+            dia2.Add(14);
+            dia3.Add(14.5);
+            dia3.Add(18.5);
+            dia3.Add(23);
+            dia3.Add(20);
+            dia3.Add(17.5);
+
+            double temperaturaMediaDia1 = 16.4;
+            double temperaturaMediaDia2 = 15.6;
+            double temperaturaMediaDia3 = 18.7;
+            double celsuiusEsperados = 17.155;
+            double farenheitEsperados = 62.879;
+            double temperaturaMaxEsperada = 23;
+            double temperaturaMinEsperada = 12.5;
+
+            miPrediccionPrueba.PredecirTemperatura(dia1, dia2, dia3);
+
+            Assert.AreEqual(celsuiusEsperados, miPrediccionPrueba.TempCelsius, 0.001, "Los valores no son correctos");
+            Assert.AreEqual(farenheitEsperados, miPrediccionPrueba.TempFarenheit, 0.001, "Los valores no son correctos");
+            Assert.AreEqual(temperaturaMaxEsperada, miPrediccionPrueba.TempMax, "Los valores no son correctos");
+            Assert.AreEqual(temperaturaMinEsperada, miPrediccionPrueba.TempMin, "Los valores no son correctos");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception),Prediccion.NoListaVacia1)]
+        public void PruebaDia1Vacio ()
+        {
+            Prediccion miPrediccionPrueba = new Prediccion();
+            List<double> dia1 = new List<double>(),
+                         dia2 = new List<double>(),
+                         dia3 = new List<double>();
+
+            dia2.Add(13); dia2.Add(15); dia2.Add(19.5); dia2.Add(16.5); dia2.Add(14);
+            dia3.Add(14.5); dia3.Add(18.5); dia3.Add(23); dia3.Add(20); dia3.Add(17.5);
+   
+            miPrediccionPrueba.PredecirTemperatura(dia1, dia2, dia3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), Prediccion.NoListaVacia2)]
+        public void PruebaDia2Vacio()
+        {
+            Prediccion miPrediccionPrueba = new Prediccion();
+
+            List<double> dia1 = new List<double>(),
+                         dia2 = new List<double>(),
+                         dia3 = new List<double>();
+
+            dia1.Add(12.5); dia1.Add(16.5); dia1.Add(21); dia1.Add(17); dia1.Add(15);
+            dia3.Add(14.5); dia3.Add(18.5); dia3.Add(23); dia3.Add(20); dia3.Add(17.5);
+
+            miPrediccionPrueba.PredecirTemperatura(dia1, dia2, dia3);
+        }
+
+        [TestMethod]
+
+        public void PruebaDia3Vacio()
+        {
+            Prediccion miPrediccionPrueba = new Prediccion();
+
+            List<double> dia1 = new List<double>(),
+                         dia2 = new List<double>(),
+                         dia3 = new List<double>();
+
+            dia1.Add(12.5); dia1.Add(16.5); dia1.Add(21); dia1.Add(17); dia1.Add(15);
+            dia2.Add(13); dia2.Add(15); dia2.Add(19.5); dia2.Add(16.5); dia2.Add(14);
+            try
+            {
+                miPrediccionPrueba.PredecirTemperatura(dia1, dia2, dia3);
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, Prediccion.NoListaVacia3);
+                return;
+            }
+            Assert.Fail("Error");
         }
     }
 }
